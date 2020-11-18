0b36c0a792a15bc9872fc0ba5a64f342f2f99be0
Lincoln Marcolongo <mlincoln1205@gmail.com>
2020-11-18 21:09:19 +0100

    Yelp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, :address, :category, presence: true
+
+  has_many :reviews, dependent: :destroy
+  validates :category, inclusion: { in: CATEGORY }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+  review = Review.create!(
+  content: "teste review",
+  rating: rand(0..5),
+  restaurant: restaurant
+  )
+end
